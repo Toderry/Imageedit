822f5cafbfa1b7f5f6574a1b2348d3466a794f99
Todery <o.rodygina2003@gmail.com>
2023-04-10 09:38:28 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imageedit
+{
+    public static class Imageeditor
+    {
+        public static List<Bitmap> Load(string path)
+        {
+            List<Bitmap> list = new List<Bitmap>();
+            var BT = new Bitmap(@"C:\Users\Андрей Родыгин\Desktop\in\Снимок экрана (1).png");
+            list.Add(BT);
+            return list;
+        }
+    }
+}
